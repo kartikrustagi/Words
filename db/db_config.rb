c9aded52caf77adefb75b535f7b225df0861912a
Kartik Rustagi <kartikrustagi@gmail.com>
2012-04-07 18:06:59 +0530

    Fix: DB config loads

--- db/db_config.rb
+++ db/db_config.rb
@@ -1,3 +1,7 @@
 db_conf = YAML::load(File.open('config/db.yaml'))
-
-DB = Sequel.connect(db_conf[db_conf["env"]])
+env = db_conf["env"]
+if env == "development"
+	DB = Sequel.connect(db_conf[db_conf["env"]])
+else
+	DB = Sequel.connect(ENV['DATABASE_URL'])
+end
